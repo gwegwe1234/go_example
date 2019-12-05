ee219026f68c3acac7168f29c297d8ab642e138c
gwegwe1234 <gwangtae5860@daum.net>
2019-12-05 15:36:39 +0900

    section2 enum

---
+++ section2/enumeration2.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main()  {
+	const (
+		A = iota + 1
+		B
+		C
+	)
+
+	fmt.Print(A, B, C)
+	// 1 2 3
+}
---
+++ section2/enumeration3.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main()  {
+	const (
+		_ = iota
+		A
+		_
+		C
+	)
+
+	const (
+		_ = iota + 0.75 * 2
+		DEFAULT
+		SILVER
+		GOLD
+		PLATINUM
+	)
+
+	fmt.Println(A, C)
+	// 1 3
+
+	fmt.Println(DEFAULT, SILVER, GOLD, PLATINUM)
+	// 2.5 3.5 4.5 5.5
+}
